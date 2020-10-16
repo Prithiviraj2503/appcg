7f5c555de98062b50a3a76268f94b1a25d0cd65a
PRITHIVIRAJ K <prithivirajk2503@gmail.com>
2020-10-16 20:47:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='text-alighn:center;color:darkblue'>Hello World!<h1>"
-    return "<h2 style='text-alighn:center;color:red'>Welcome To Cloud Computing Lab!<h2>"
+    return "<h1 style='text-align:center;color:darkblue'>Hello World!<h1>"
+    return "<h2 style='text-align:center;color:red'>Welcome To Cloud Computing Lab!<h2>"
 
 if __name__ == "__main__":
     app.run()
